b365ff818bf5b5f4dd590a5ab6f497ac31385731
Samuele Trainotti <Samuele.Trainotti@marconi.loc>
2018-10-10 15:53:20 +0200

    lezione 10 ottobre

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
 var express = require("express");
-var path = require("path");
-var fs = require("fs");
+var http = require("http");
+var util= require("util");
+
 var app = express();
-app.use(function(req, res, next) {
-    console.log("Request IP: " + req.url);
-    console.log("Request date: " + new Date());
+
+app.get("/users/:userid", function(req, res) {
+    var userid = parseInt(req.params.userid, 10);
+    res.writeHead(200, {"Content-Type":"text/html"});
+    console.log(userid);
+    res.end("The user id is: " + userid);
+})
+
+app.use(function(request, response, next) {
+    //console.log("In comes a " + request.method + " to " + request.url);
+    //console.log(util.inspect(request, {showHidden: false, depth: null}));
+   // console.log(util.inspect(request.query));
+      var query = request.query;
+    
+      query.p=query.p+"test";
+      console.log(query.p);
+      console.log(query.p2);
+      
+      next();
 });
-app.listen(3000, function() {
-    console.log("App started on port 3000");
+
+app.use(function(request, response, next) {
+    console.log("Secondo mid " + request.method + " to " + request.url);
+    next();
 });
+
+app.use(function(request, response) {
+    var query = request.query;
+    //response.writeHead(200, { "Content-Type": "text/plain" });
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.end("Hello, world! <br>"+query.p);
+});
+
+http.createServer(app).listen(3000);
